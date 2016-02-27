062d37c29094c774ce7772c5cc66a0d0461e6cdd
Vybhatt95 <V_Bhatt@uncg.edu>
2016-02-27 07:25:01 -0500

    Added inital express server

---
+++ mysql.js
@@ -0,0 +1,21 @@
+var mysql      = require('mysql');
+
+function MySqlClient() {
+	this.connection = mysql.createConnection({
+  		host     : 'localhost',
+ 		user     : 'me',
+ 		password : 'secret', 
+	  	database : 'my_db'
+	});
+	this.connection.connect();
+}
+
+MySqlClient.prototype.getConnection = function (query, cb) {
+	return this.connection;
+}
+
+MySqlClient.prototype.endConnection = function(first_argument) {
+	this.connection.end();
+};
+
+module.exports = MySqlClient;
